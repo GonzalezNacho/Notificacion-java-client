3ec965fe7a3f641e80e015a93ef2376d030de761
GonzalezNacho <nacho.ldm63@gmail.com>
2024-04-04 18:43:37 -0300

    primer commit

---
+++ clientSocketIO/src/main/java/com/gonzalez/nacho/clientSocketIO/App.java
@@ -0,0 +1,50 @@
+package com.gonzalez.nacho.clientSocketIO;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+import java.net.URISyntaxException;
+
+/**
+ * Cliente de socket io
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try {
+            // Conectar al servidor Socket.IO
+            Socket socket = IO.socket("http://localhost:3000");
+
+            // Escuchar eventos desde el servidor
+            socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    System.out.println("Conectado al servidor");
+                }
+            }).on("message", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    String titulo = (String) args[0];
+                    String mensaje = (String) args[1];
+                    System.out.println("Titulo: " + titulo);
+                    System.out.println("Mensaje del servidor: " + mensaje);
+                }
+            });
+
+            // Conectar al servidor
+            socket.connect();
+
+            // Esperar unos segundos antes de desconectar
+            Thread.sleep(50000);
+
+            // Desconectar del servidor
+            //socket.disconnect();
+
+        } catch (URISyntaxException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
